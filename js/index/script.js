c826c5bb4c1dc2e6660455964c2f1b107b341c23
Hgnim <hgn1m@outlook.com>
2025-01-11 18:23:16 +0800

    完成toast基本代码，并完成demo网页

---
+++ js/index/script.js
@@ -0,0 +1,12 @@
+function n1() {
+  showToast("hello!");
+}
+function n2() {
+  showToast("other colors", 3000, "button-2");
+}
+function n3() {
+  showToast("inside the div", 3500, "button-3", undefined, "buttonDiv");
+}
+function n4() {
+  showToast("big text", 1000, undefined, "button-4");
+}
