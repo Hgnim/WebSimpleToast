8c115f265a1e56e3c0ab41b33475e4fd9dd17922
Hgnim <hgn1m@outlook.com>
2025-01-13 13:23:18 +0800

    更新demo内容

--- demo/js/script.js
+++ demo/js/script.js
@@ -10,3 +10,10 @@ function n3() {
 function n4() {
   showToast("big text", 1000, undefined, "button-4");
 }
+function n5() {
+  showToast("1", 500, undefined, undefined, undefined, function () {
+    showToast("2", 500, undefined, undefined, undefined, function () {
+      showToast("3", 500);
+    });
+  });
+}
